9518aceed944cb1355098bee1194473274d66698
roxyvallee <rvallee@etud.u-pem.fr>
2020-01-15 15:20:59 +0100

    [UNITY] add button

---
+++ Explora_Defense/Assets/UIHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+    public GameObject img;
+    public Image nav;
+    
+    public void buttonEvent()
+    {
+        if(img.active)
+        {
+            img.SetActive(false);
+            
+        }
+        else
+        {
+            img.SetActive(true);
+        }
+        
+        //img2.SetActive(true);
+        //Debug.Log("Hello World"); 
+    }
+    
+    
+    
+     public void navStart()
+    {
+        nav.gameObject.SetActive(true);     
+        nav.fillAmount = 1.0f;
+    }
+    
+}
