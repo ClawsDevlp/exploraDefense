adc05dc8710fefb43d9197eaaa716ccf55d68276
roxyvallee <rvallee@etud.u-pem.fr>
2020-01-15 17:57:53 +0100

    [UNITY] Timer

---
+++ Explora_Defense/Assets/TimeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public float startingTime;
+    private Text theText;
+    void Start()
+    {
+        theText = GetComponent<Text>();
+        startingTime *= 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startingTime = startingTime - Time.deltaTime;
+        theText.text = "" + Mathf.Round(startingTime/60) + " min " + Mathf.Round(startingTime/3);
+    }
+}
