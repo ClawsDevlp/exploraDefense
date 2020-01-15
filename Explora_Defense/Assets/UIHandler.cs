26e68d7c16835826557a43d7a4e856d3a2fd99e7
roxyvallee <rvallee@etud.u-pem.fr>
2020-01-15 16:53:55 +0100

    [UNITY] create transition page

--- Explora_Defense/Assets/UIHandler.cs
+++ Explora_Defense/Assets/UIHandler.cs
@@ -7,6 +7,8 @@ public class UIHandler : MonoBehaviour
 {
     public GameObject img;
     public Image nav;
+    public Image oui;
+    public Image non;
     
     public void buttonEvent()
     {
@@ -28,8 +30,34 @@ public class UIHandler : MonoBehaviour
     
      public void navStart()
     {
-        nav.gameObject.SetActive(true);     
-        nav.fillAmount = 1.0f;
+        if(!nav.gameObject.active)
+        {
+            nav.gameObject.SetActive(true);     
+            //nav.fillAmount = 1.0f;
+            StartCoroutine(startSlide(1.0f));
+        }
+        else if(nav.gameObject.active)
+        {    
+            //nav.fillAmount = 1.0f;
+            StartCoroutine(startSlide(-1.0f));
+        }
+        
+    }
+
+    IEnumerator startSlide(float i)
+    {
+        yield return new WaitForSeconds(0.0001f);
+        nav.fillAmount = nav.fillAmount + i; 
+        oui.fillAmount = oui.fillAmount + i;
+        non.fillAmount = non.fillAmount + i;
+        //Debug.Log("Test 123");
+        if(nav.fillAmount < 1 && nav.fillAmount > 0 )
+            StartCoroutine(startSlide(i));
+        else if( nav.fillAmount == 0)
+        {
+            nav.gameObject.SetActive(false);
+        }
+            
     }
     
 }
