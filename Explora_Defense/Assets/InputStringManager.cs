00cfa6a1064723ea940d7261bd4b7d7bf3ae7e78
Unknown <rvallee@etud.u-pem.fr>
2020-01-21 22:58:04 +0100

    [UNITY] page 12-19

---
+++ Explora_Defense/Assets/InputStringManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputStringManager : MonoBehaviour
+{
+    public InputField newCode;
+    public string code;
+
+    public void Text_Changed(Image newPage)
+    {
+        string temp = newCode.text;
+        if(code.CompareTo(temp) == 0)
+        {
+            GetComponent<UIHandler>().changePage(newPage);
+        } else {
+            // Faire message erreur utilisateur
+            Debug.Log("NUL World"); 
+        }
+    }
+   
+}
