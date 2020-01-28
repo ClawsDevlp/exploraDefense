c160e9508978217167babc5459b5225b4f08be32
Clara <clara.daigmorte@gmail.com>
2020-01-28 12:13:02 +0100

    [MERGE] + [DEV] debug qr code

--- Explora_Defense/Assets/QRCodeHandler.cs
+++ Explora_Defense/Assets/QRCodeHandler.cs
@@ -10,8 +10,10 @@ public class QRCodeHandler : MonoBehaviour
 {
     private WebCamTexture camTexture;
     private Rect screenRect;
+    public Image thisPage;
     public Image newPage;
     private bool useGui = true;
+    IBarcodeReader barcodeReader;
 
     void Start() {
         screenRect = new Rect(0, 0, Screen.width, Screen.height);
@@ -26,19 +28,27 @@ public class QRCodeHandler : MonoBehaviour
     void OnGUI () {
         if(useGui){
             // drawing the camera on screen
-            GUI.DrawTexture (screenRect, camTexture, ScaleMode.ScaleToFit);
+            GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
             // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
             try {
-                IBarcodeReader barcodeReader = new BarcodeReader ();
+                barcodeReader = new BarcodeReader ();
                 // decode the current frame
                 var result = barcodeReader.Decode(camTexture.GetPixels32(), camTexture.width , camTexture.height);
                 if (result != null) {
-                    if(result.Text == "https://equipeludique.fr/nos-jeux/"){
-                        useGui = false;
-                        GetComponent<UIHandler>().changePage(newPage);
+                    if(result.Text == "https://equipeludique.fr/nos-jeux/"){    
+                        stopAll();
                     }
                 }
             } catch(Exception ex) { Debug.LogWarning (ex.Message); }
         }
+    }
+
+    void stopAll() {
+        camTexture.Stop();
+        useGui = false;
+        barcodeReader = null;
+        camTexture = null;
+        GetComponent<UIHandler>().changePage(newPage);
+        thisPage.gameObject.SetActive(false);
     }
 }
