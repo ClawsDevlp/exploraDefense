5be3d5557a1e1847fb8c21dff2fb1e37d6336647
Unknown <rvallee@etud.u-pem.fr>
2020-01-29 00:35:05 +0100

    [DEV] add text writing effect

---
+++ Explora_Defense/Assets/TexteAnimation.cs
@@ -0,0 +1,27 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TexteAnimation : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public Text fullText;
+    public string sentence;
+    private Text currentText;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+    IEnumerator Type()
+    {
+        foreach(char letter in sentence.ToCharArray() )
+        {
+            fullText.text += letter;
+            yield return new WaitForSeconds(speed);
+        }
+    }
+
+    
+}
